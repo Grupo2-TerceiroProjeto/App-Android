c48895fbaa2cfc4439fc9577c9f1e746970b86c1
EmmilyferreiraDeJesus <emmily.jesus@sptech.school>
2025-02-21 16:16:42 -0300

    Organização de pastas + Configuração de Cores

--- app/src/main/java/com/example/gestok/ui/theme/Color.kt
+++ app/src/main/java/com/example/gestok/ui/theme/Color.kt
@@ -2,10 +2,9 @@ package com.example.gestok.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Blue = Color(0xFF196BAD)
+val DarkBlue = Color(0xFF005BA4)
+val LightBlue = Color(0xFF26A2C6)
+val White = Color(0xFFFFFFFF)
+val Black = Color(0XFF4A4A4A)
+val Gray = Color(0xFFD9D9D9)
--- app/src/main/java/com/example/gestok/ui/theme/Theme.kt
+++ app/src/main/java/com/example/gestok/ui/theme/Theme.kt
@@ -11,16 +11,10 @@ import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
-)
-
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = Blue,
+    secondary = DarkBlue,
+    tertiary = LightBlue
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -46,7 +40,6 @@ fun GestokTheme(
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
 
-        darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
 
