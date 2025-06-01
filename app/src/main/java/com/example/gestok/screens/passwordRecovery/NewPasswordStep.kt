fda892978b24445c767b1dae6f7473d24a08071a
EmmilyferreiraDeJesus <emmily.jesus@sptech.school>
2025-05-31 23:37:37 -0300

    feat: Integração de Redefinição de senha

--- app/src/main/java/com/example/gestok/screens/passwordRecovery/NewPasswordStep.kt
+++ app/src/main/java/com/example/gestok/screens/passwordRecovery/NewPasswordStep.kt
@@ -2,27 +2,42 @@ package com.example.gestok.screens.passwordRecovery
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.gestok.components.Input
 import com.example.gestok.components.PrimaryButton
+import com.example.gestok.screens.login.data.UserStepReset
+import com.example.gestok.viewModel.login.LoginApiViewModel
 
 @Composable
-fun NewPasswordStep(navController: NavController) {
+fun NewPasswordStep(
+    navController: NavController,
+    viewModel: LoginApiViewModel
+) {
 
     var senha by remember { mutableStateOf("") }
     var confirmarSenha by remember { mutableStateOf("") }
+
+    LaunchedEffect(Unit) {
+        viewModel.limparErros()
+    }
 
     Column(
         modifier = Modifier
@@ -44,7 +59,11 @@ fun NewPasswordStep(navController: NavController) {
                     senha = it
                 }
             },
-            visualTransformation = PasswordVisualTransformation()
+            visualTransformation = PasswordVisualTransformation(),
+            isError = viewModel.senhaErro != null,
+            supportingText = {
+                viewModel.senhaErro?.let { Text(text = it) }
+            }
         )
         Input(
             "Confirmar senha",
@@ -58,9 +77,22 @@ fun NewPasswordStep(navController: NavController) {
                     confirmarSenha = it
                 }
             },
-            visualTransformation = PasswordVisualTransformation())
+            visualTransformation = PasswordVisualTransformation(),
+            isError = viewModel.confirmarSenhaErro != null,
+            supportingText = {
+                viewModel.confirmarSenhaErro?.let { Text(text = it) }
+            }
+        )
 
-        PrimaryButton ("Concluir") { navController.navigate("login") }
+        PrimaryButton("Concluir") {
+            val email = viewModel.emailResponse.value?.email ?: return@PrimaryButton
+
+            viewModel.redefinirSenha(
+                UserStepReset(email, senha, confirmarSenha)
+            ) {
+                navController.navigate("login")
+            }
+        }
 
     }
 
--- app/src/main/java/com/example/gestok/viewModel/login/LoginApiViewModel.kt
+++ app/src/main/java/com/example/gestok/viewModel/login/LoginApiViewModel.kt
@@ -5,7 +5,11 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.viewModelScope
 import com.example.gestok.network.service.AuthService
 import com.example.gestok.screens.login.data.LoginUser
+import com.example.gestok.screens.login.data.SendEmail
+import com.example.gestok.screens.login.data.SendEmailResponse
+import com.example.gestok.screens.login.data.UserReset
 import com.example.gestok.screens.login.data.UserSession
+import com.example.gestok.screens.login.data.UserStepReset
 import kotlinx.coroutines.launch
 import retrofit2.HttpException
 
@@ -60,6 +64,83 @@ class LoginApiViewModel(
                 }
             } catch (e: Exception) {
                 _senhaErro = "Erro ao conectar ao servidor"
+                Log.e("API", "Erro ao conectar ao servidor: ${e.message}")
+            }
+        }
+    }
+
+    override fun enviarEmail(email: String, onSuccess: () -> Unit) {
+        limparErros()
+
+        var houveErro = false
+
+        if (email.isBlank()) {
+            _emailErro = "E-mail é obrigatório"
+            houveErro = true
+        } else if(!email.matches(Regex("^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$"))) {
+            _emailErro = "E-mail inválido"
+            houveErro = true
+        }
+
+        if (houveErro) return
+
+        viewModelScope.launch {
+            try {
+                val emailRequest = SendEmail(
+                    email = email
+                )
+
+                val response = api.enviarEmail(emailRequest)
+                _emailResponse.value = SendEmailResponse(response.email, response.codigo)
+                onSuccess()
+                Log.d("API", "E-mail enviado com sucesso. Email: ${_emailResponse.value?.email}, Código: ${_emailResponse.value?.codigo}")
+            } catch (e: HttpException) {
+                if (e.code() == 500) {
+                    _emailNaoEncontrado = "Enviaremos instruções de redefinição de senha por e-mail em breve"
+                    Log.e("API", "Email não encontrado ${e.message}")
+                }
+            } catch (e: Exception) {
+                Log.e("API", "Erro ao conectar ao servidor ${e.message}")
+            }
+        }
+    }
+
+    override fun redefinirSenha(usuario: UserStepReset, onSuccess: () -> Unit) {
+        limparErros()
+
+        var houveErro = false
+
+        if (usuario.senha.isBlank()) {
+            _senhaErro = "Nova senha é obrigatória"
+            houveErro = true
+        }
+
+        if (usuario.senha != usuario.confirmarSenha) {
+            _confirmarSenhaErro = "As senhas não coincidem"
+            houveErro = true
+        }
+
+        if (houveErro) return
+
+        viewModelScope.launch {
+            try {
+                val usuarioRedefinicao = UserReset(
+                    email = usuario.email,
+                    senha = usuario.senha
+                )
+
+                val response = api.redefinirSenha(usuarioRedefinicao)
+
+                if (response.isSuccessful) {
+                    onSuccess()
+                    Log.d("API", "Senha redefinida com sucesso")
+                } else {
+                    Log.e("API", "Erro ao redefinir senha: ${response.code()}")
+                }
+
+            } catch (e: HttpException) {
+                Log.e("API", "Erro ao redefinir senha: ${e.message}")
+            } catch (e: Exception) {
                 Log.e("API", "Erro ao conectar ao servidor: ${e.message}")
             }
         }
